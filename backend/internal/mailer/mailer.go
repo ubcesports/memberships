b1c50fe3218a6e7a9d80d2e558045a8a8c73b168
SudiMango <sudipto.islam2005@gmail.com>
2026-06-04 13:45:20 -0700

    Added documentation to mailer package

--- backend/internal/mailer/mailer.go
+++ backend/internal/mailer/mailer.go
@@ -11,6 +11,11 @@ import (
 var client *resend.Client
 var sender string
 
+// Initialize the mailing service. To be called only once at application startup
+//
+// Requirements from .env file:
+//   - RESEND_API_KEY: API key from Resend
+//   - SENDER_EMAIL: Name and email of sender (eg. "Sudi from UBCEA <sudi@ubcesports.ca>")
 func Init() {
 	resend_api_key := os.Getenv("RESEND_API_KEY")
 	if resend_api_key == "" {
@@ -23,6 +28,20 @@ func Init() {
 	client = resend.NewClient(resend_api_key)
 }
 
+// Dispatches an HTML email to one or more recipients asynchronously
+//
+// Params:
+//   - to: list of recipients of mail
+//   - subject: subject of mail
+//   - htmlContent: content of mail in html format
+//
+// Example usage:
+//
+//	mailer.SendEmailAsync(
+//		[]string{"user@example.com"},
+//		"Welcome to our platform!",
+//		"<h1>Thanks for signing up!</h1>",
+//	)
 func SendEmailAsync(to []string, subject string, htmlContent string) {
 	go func() {
 		params := &resend.SendEmailRequest{
