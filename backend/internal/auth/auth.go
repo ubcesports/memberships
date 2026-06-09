8c9bb9c1b55e16b7f6d63ee0643d0992fddb62e5
SudiMango <sudipto.islam2005@gmail.com>
2026-06-08 20:59:29 -0700

    switched auth from google to jasperlabs

--- backend/internal/auth/auth.go
+++ backend/internal/auth/auth.go
@@ -9,7 +9,8 @@ import (
 	"github.com/thecodearcher/limen"
 	sqladapter "github.com/thecodearcher/limen/adapters/sql"
 	"github.com/thecodearcher/limen/plugins/oauth"
-	oauthgoogle "github.com/thecodearcher/limen/plugins/oauth-google"
+
+	oauthgeneric "github.com/thecodearcher/limen/plugins/oauth-generic"
 )
 
 type uuidGenerator struct{}
@@ -29,9 +30,29 @@ func NewLimen(db *sql.DB) (*limen.Limen, error) {
 		Schema:   schema,
 		Plugins: []limen.Plugin{
 			oauth.New(
-				oauth.WithProviders(oauthgoogle.New(
-					oauthgoogle.WithRedirectURL(os.Getenv("GOOGLE_CALLBACK_URI")),
-				)),
+				oauth.WithProviders(
+					oauthgeneric.New(
+						oauthgeneric.WithName("jasperlabs"),
+						oauthgeneric.WithClientID(os.Getenv("OAUTH_CLIENT_ID")),
+						oauthgeneric.WithClientSecret(os.Getenv("OAUTH_CLIENT_SECRET")),
+						oauthgeneric.WithRedirectURL(os.Getenv("OAUTH_CALLBACK_URL")),
+						oauthgeneric.WithScopes("openid", "profile", "email"),
+
+						oauthgeneric.WithDiscoveryURL("https://auth.jasperlabs.net/.well-known/openid-configuration"),
+
+						oauthgeneric.WithMapUserInfo(func(raw map[string]any) (*oauth.ProviderUserInfo, error) {
+							id, _ := raw["sub"].(string)
+							name, _ := raw["name"].(string)
+							email, _ := raw["email"].(string)
+
+							return &oauth.ProviderUserInfo{
+								ID:    id,
+								Name:  name,
+								Email: email,
+							}, nil
+						}),
+					),
+				),
 				oauth.WithMapProfileToUser(func(info *limen.OAuthAccountProfile) map[string]any {
 					return map[string]any{
 						"full_name": info.Name,
